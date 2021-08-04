bc8969592c5a4be969d1738fe24a0833910e44f3
swapan <swapan.d83@gmail.com>
2021-08-04 08:19:06 +0600

    Setting up the model for Room database (column name)

--- app/src/main/java/com/example/dogsappjetpack/model/DogBreed.java
+++ app/src/main/java/com/example/dogsappjetpack/model/DogBreed.java
@@ -1,5 +1,8 @@
 package com.example.dogsappjetpack.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.PrimaryKey;
+
 import com.google.gson.annotations.SerializedName;
 
 /**
@@ -10,28 +13,34 @@ public class DogBreed {
     // use the same name of the variable that have in API, otherwise use below mention
     // @SerilizedName("") annotation
 
+    @ColumnInfo(name = "breed_id")
     @SerializedName("id")
     public String breedId;
 
+    @ColumnInfo(name = "dog_name")
     @SerializedName("name")
     public String dogBreed;
 
+    @ColumnInfo(name = "life_span")
     @SerializedName("life_span")
     public String lifeSpan;
 
+    @ColumnInfo(name = "breed_group")
     @SerializedName("breed_group")
     public String breedGroup;
 
+    @ColumnInfo(name = "fred_for")
     @SerializedName("bred_for")
     public String bredFor;
 
-    public String temperament;
+    public String temperament;  // No need to annotation because we used same name as API
 
+    @ColumnInfo(name = "dog_url")
     @SerializedName("url")
     public String imageUrl;
 
-    // It is not as per API service, we will use it for database(Room Database
-    public int uuid;
+    @PrimaryKey(autoGenerate = true)
+    public int uuid;  // It is not as per API service, we will use it for database(Room Database
 
     // Constructor for the above mentioned attribute of dog that will come from API
     public DogBreed(String breedId, String dogBreed, String lifeSpan, String breedGroup,
