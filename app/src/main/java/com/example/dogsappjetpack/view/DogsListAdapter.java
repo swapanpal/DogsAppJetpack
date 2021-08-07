02bf691462cfc47c0cce2b715d7040ea569600d5
swapan <swapan.d83@gmail.com>
2021-08-07 11:41:03 +0600

    Add ClickListener to the list to open detail screen

--- app/src/main/java/com/example/dogsappjetpack/view/DogsListAdapter.java
+++ app/src/main/java/com/example/dogsappjetpack/view/DogsListAdapter.java
@@ -1,12 +1,15 @@
 package com.example.dogsappjetpack.view;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dogsappjetpack.R;
@@ -44,12 +47,19 @@ public class DogsListAdapter extends RecyclerView.Adapter<DogsListAdapter.DogVie
         ImageView image = holder.itemView.findViewById(R.id.imageView);
         TextView name = holder.itemView.findViewById(R.id.name);
         TextView lifespan = holder.itemView.findViewById(R.id.lifespan);
+        LinearLayout layout = holder.itemView.findViewById(R.id.dogLayout);
 
         name.setText(dogsList.get(position).dogBreed);
         lifespan.setText(dogsList.get(position).lifeSpan);
         // Code the below line after creating Util class for Glide
         // This line of code load image from API by using Glide library
         Util.loadImage(image, dogsList.get(position).imageUrl, Util.getProgressDrawable(image.getContext()));
+       // set a clickListener to this view (new View.OnClickListener) to go detail
+        layout.setOnClickListener(view -> {
+            ListFragmentDirections.ActionDetail action = ListFragmentDirections.actionDetail();
+            action.setDogUuid(dogsList.get(position).uuid);
+            Navigation.findNavController(layout).navigate(action);
+        });
     }
 
     @Override
