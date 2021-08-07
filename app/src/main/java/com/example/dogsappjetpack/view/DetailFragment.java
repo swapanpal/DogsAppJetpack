99aa743a284ae04da47a047907dbc54666b08e10
swapan <swapan.d83@gmail.com>
2021-08-07 12:22:14 +0600

    Add Detail screen Info & Image from database

--- app/src/main/java/com/example/dogsappjetpack/view/DetailFragment.java
+++ app/src/main/java/com/example/dogsappjetpack/view/DetailFragment.java
@@ -9,6 +9,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +19,7 @@ import android.widget.TextView;
 
 import com.example.dogsappjetpack.R;
 import com.example.dogsappjetpack.model.DogBreed;
+import com.example.dogsappjetpack.util.Util;
 import com.example.dogsappjetpack.viewmodel.DetailViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -68,18 +70,22 @@ public class DetailFragment extends Fragment {
             dogUuid = DetailFragmentArgs.fromBundle(getArguments()).getDogUuid();
         }
         viewModel = ViewModelProviders.of(this).get(DetailViewModel.class);
-        viewModel.fetch();
+        viewModel.fetch(dogUuid);
 
         // call the observeViewModel
         observeViewModel();
     }
     private void observeViewModel(){
         viewModel.dogLiveData.observe(getViewLifecycleOwner(), dogBreed -> {
-        if (dogBreed != null && dogBreed instanceof DogBreed){
+        if (dogBreed != null && dogBreed instanceof DogBreed && getContext() != null){
             dogName.setText(dogBreed.dogBreed);
             dogPurpose.setText(dogBreed.bredFor);
             dogTemperament.setText(dogBreed.temperament);
             dogLifespan.setText(dogBreed.lifeSpan);
+            // For loading actual image
+            if (dogBreed.imageUrl != null){
+                Util.loadImage(dogImage, dogBreed.imageUrl,new CircularProgressDrawable(getContext()));
+            }
         }
         });
     }
--- app/src/main/java/com/example/dogsappjetpack/viewmodel/DetailViewModel.java
+++ app/src/main/java/com/example/dogsappjetpack/viewmodel/DetailViewModel.java
@@ -1,28 +1,64 @@
 package com.example.dogsappjetpack.viewmodel;
 
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.dogsappjetpack.model.DogBreed;
+import com.example.dogsappjetpack.model.DogDatabase;
 
 /**
 *If we did not need to use Context than use only "ViewModel" otherwise use "AndroidViewModel"
  *One ViewModel can display only one view, example "ListFragment", If we want to display another view than
 *replace it with existing view.
  */
-public class DetailViewModel extends ViewModel {
+public class DetailViewModel extends AndroidViewModel {
 
     // Create some variable of MutableLiveData with data type, example "<List<DogBreed>>"
     // MutableLiveData means it is changeable of it's value.
     public MutableLiveData<DogBreed> dogLiveData = new MutableLiveData<DogBreed>();
+    private RetrieveDogTask task;
+
+    public DetailViewModel(@NonNull Application application) {
+        super(application);
+    }
 
 
     /**
      * This method will fetch the data from database(Room database) but currently we use fake data
      */
-    public void fetch(){
-        DogBreed dog1 = new DogBreed("1", "corgi", "15 years",
-                "","Companionship","Calm and friendly","");
-        dogLiveData.setValue(dog1);
+    public void fetch(int uuid){
+        task = new RetrieveDogTask();
+        task.execute(uuid);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        if (task != null){
+            task.cancel(true);
+            task = null;
+        }
+    }
+
+    /**
+     * Create a AsyncTask class to retrieve data as background task
+     */
+    private class RetrieveDogTask extends AsyncTask<Integer, Void, DogBreed>{
+
+        @Override
+        protected DogBreed doInBackground(Integer... integers) {
+            int uuid = integers[0];
+            return DogDatabase.getInstance(getApplication()).dogDao().getDog(uuid);
+        }
+
+        @Override
+        protected void onPostExecute(DogBreed dogBreed) {
+            dogLiveData.setValue(dogBreed);
+        }
     }
 }
