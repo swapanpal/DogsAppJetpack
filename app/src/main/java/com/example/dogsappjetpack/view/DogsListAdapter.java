46ab38aab800c990af4b6bd8e73e3e523f4cc923
swapan <swapan.d83@gmail.com>
2021-09-04 17:48:30 +0600

    Binding the Dog item (Data Binding)

--- app/src/main/java/com/example/dogsappjetpack/view/DogsListAdapter.java
+++ app/src/main/java/com/example/dogsappjetpack/view/DogsListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.dogsappjetpack.view;
 
+import android.content.ClipData;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,12 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dogsappjetpack.R;
+import com.example.dogsappjetpack.databinding.ItemDogBinding;
 import com.example.dogsappjetpack.model.DogBreed;
 import com.example.dogsappjetpack.util.Util;
 
@@ -37,29 +40,31 @@ public class DogsListAdapter extends RecyclerView.Adapter<DogsListAdapter.DogVie
     @NonNull
     @Override
     public DogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(
-                R.layout.item_dog, parent, false);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        ItemDogBinding view = DataBindingUtil.inflate(inflater, R.layout.item_dog, parent, false);
         return new DogViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull DogViewHolder holder, int position) {
-        ImageView image = holder.itemView.findViewById(R.id.imageView);
-        TextView name = holder.itemView.findViewById(R.id.name);
-        TextView lifespan = holder.itemView.findViewById(R.id.lifespan);
-        LinearLayout layout = holder.itemView.findViewById(R.id.dogLayout);
-
-        name.setText(dogsList.get(position).dogBreed);
-        lifespan.setText(dogsList.get(position).lifeSpan);
-        // Code the below line after creating Util class for Glide
-        // This line of code load image from API by using Glide library
-        Util.loadImage(image, dogsList.get(position).imageUrl, Util.getProgressDrawable(image.getContext()));
-       // set a clickListener to this view (new View.OnClickListener) to go detail
-        layout.setOnClickListener(view -> {
-            ListFragmentDirections.ActionDetail action = ListFragmentDirections.actionDetail();
-            action.setDogUuid(dogsList.get(position).uuid);
-            Navigation.findNavController(layout).navigate(action);
-        });
+        holder.itemView.setDog(dogsList.get(position));
+        
+//        ImageView image = holder.itemView.findViewById(R.id.imageView);
+//        TextView name = holder.itemView.findViewById(R.id.name);
+//        TextView lifespan = holder.itemView.findViewById(R.id.lifespan);
+//        LinearLayout layout = holder.itemView.findViewById(R.id.dogLayout);
+//
+//        name.setText(dogsList.get(position).dogBreed);
+//        lifespan.setText(dogsList.get(position).lifeSpan);
+//        // Code the below line after creating Util class for Glide
+//        // This line of code load image from API by using Glide library
+//        Util.loadImage(image, dogsList.get(position).imageUrl, Util.getProgressDrawable(image.getContext()));
+//       // set a clickListener to this view (new View.OnClickListener) to go detail
+//        layout.setOnClickListener(view -> {
+//            ListFragmentDirections.ActionDetail action = ListFragmentDirections.actionDetail();
+//            action.setDogUuid(dogsList.get(position).uuid);
+//            Navigation.findNavController(layout).navigate(action);
+//        });
     }
 
     @Override
@@ -68,9 +73,9 @@ public class DogsListAdapter extends RecyclerView.Adapter<DogsListAdapter.DogVie
     }
 
     public class DogViewHolder extends RecyclerView.ViewHolder {
-        public View itemView;
-        public DogViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public ItemDogBinding itemView;
+        public DogViewHolder(@NonNull ItemDogBinding itemView) {
+            super(itemView.getRoot());
             this.itemView = itemView;
         }
     }
