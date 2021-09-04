44ff70daf3be419fedcbe49219ddaef5cd2c7d6f
swapan <swapan.d83@gmail.com>
2021-09-05 02:11:15 +0600

    Binding the detail fragment

--- app/src/main/java/com/example/dogsappjetpack/view/DetailFragment.java
+++ app/src/main/java/com/example/dogsappjetpack/view/DetailFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -18,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.dogsappjetpack.R;
+import com.example.dogsappjetpack.databinding.FragmentDetailBinding;
 import com.example.dogsappjetpack.model.DogBreed;
 import com.example.dogsappjetpack.util.Util;
 import com.example.dogsappjetpack.viewmodel.DetailViewModel;
@@ -32,21 +34,8 @@ public class DetailFragment extends Fragment {
     // create some variable
     private int dogUuid;
     private DetailViewModel viewModel;
+    private FragmentDetailBinding binding;
 
-    @BindView(R.id.dogImage)
-    ImageView dogImage;
-
-    @BindView(R.id.dogName)
-    TextView dogName;
-
-    @BindView(R.id.dogPurpose)
-    TextView dogPurpose;
-
-    @BindView(R.id.dogTemperament)
-    TextView dogTemperament;
-
-    @BindView(R.id.dogLifespan)
-    TextView dogLifespan;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -56,10 +45,11 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_detail, container, false);
-        ButterKnife.bind(this, view);
-        return view;
+        // Inflate the layout for this fragment by using DataBinding technology
+        FragmentDetailBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail,
+                container, false);
+        this.binding = binding;
+        return binding.getRoot();
     }
     // override the onViewCreated and add click listener on fab button
 
@@ -78,14 +68,7 @@ public class DetailFragment extends Fragment {
     private void observeViewModel(){
         viewModel.dogLiveData.observe(getViewLifecycleOwner(), dogBreed -> {
         if (dogBreed != null && dogBreed instanceof DogBreed && getContext() != null){
-            dogName.setText(dogBreed.dogBreed);
-            dogPurpose.setText(dogBreed.bredFor);
-            dogTemperament.setText(dogBreed.temperament);
-            dogLifespan.setText(dogBreed.lifeSpan);
-            // For loading actual image
-            if (dogBreed.imageUrl != null){
-                Util.loadImage(dogImage, dogBreed.imageUrl,new CircularProgressDrawable(getContext()));
-            }
+            binding.setDog(dogBreed);
         }
         });
     }
